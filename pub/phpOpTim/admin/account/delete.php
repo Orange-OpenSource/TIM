cfdf6c94f7c0da409748d32eb0d1e8c5f3f7a3dc
Louis <louisphilippe.sondeck@orange.com>
2015-04-22 17:56:56 +0200

    Ajout du projet TIM

--- pub/phpOpTim/admin/account/delete.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$id = (int) $_GET['id'];
-mysql_query("DELETE FROM `account` WHERE `id` = '$id' ") ;
-echo (mysql_affected_rows()) ? "Row deleted.<br /> " : "Nothing deleted.<br /> "; 
-?> 
-
-<a href='index.php?action=list'>Back To Listing</a>
--- pub/phpOpTim/admin/client/delete.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$id = (int) $_GET['id'];
-mysql_query("DELETE FROM `client` WHERE `id` = '$id' ") ; 
-echo (mysql_affected_rows()) ? "Row deleted.<br /> " : "Nothing deleted.<br /> "; 
-?> 
-
-<a href='index.php?action=list'>Back To Listing</a>
